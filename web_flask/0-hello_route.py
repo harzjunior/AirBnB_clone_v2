146464955fbff511cffeef94bafc26650bbab4d4
Haruna Bah <harunjibs@gmail.com>
2023-12-26 08:38:35 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,17 @@
 """
 from flask import Flask
 
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """prints "Hello HBNB!"
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
